088f3b6bd855faacd351f5602408a5e2431eb8c3
Sarah P <poultonse@gmail.com>
2025-01-25 15:41:49 -0600

    attempting to style the "gallery" front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import styles from "./page.module.css";
 import globalStyles from "./globals.css";
 import { useScroll } from "./scroll";
 import Daphne from '../../public/images/Daphne.jpg'
+import susanna_eyes from '../../public/images/susanna_eyes.jpg'
+import leave_blood from '../../public/images/leave-blood.jpg'
+import yellow_christ from '../../public/images/yellow_christ.jpg'
 
 export default function Home() {
     useScroll();
@@ -38,11 +41,12 @@ export default function Home() {
                         placeholder="blur"
                         quality={100}
                         // responsive={"true"}
-                        sizes={"(max-width: 64rem) 25vw, 90vh"}
-                        // style={{
-                        //     width: "100%",
-                        //     height: "100%",
-                        // }}
+                        // sizes={"(max-width: 48rem) 50vw, 100vh"}
+                        style={{
+                            height: "100%",
+                            width: "auto",
+                        }}
+                        // objectFit="contain"
                         src={Daphne}
                         // src={'/images/Daphne.jpg'}
                         // width={'3912'}
@@ -54,23 +58,62 @@ export default function Home() {
             </div>
             <div className={styles.fullScreenImageContainer}>
                 <div className={styles.fullScreenImage}>
-                    IMAGE TWO
+                    <Image
+                        placeholder="blur"
+                        quality={50}
+                        responsive={"true"}
+                        // sizes="100vw"
+                        // style={{
+                        //     width: "100%",
+                        //     // height: "100",
+                        // }}
+                        style={{
+                            height: "100%",
+                            width: "auto",
+                        }}
+                        // objectFit="contain"
+                        src={susanna_eyes}
+                        alt={"a woman bathing on the right side of image with a multitude of hands and eyes grasping for her"}
+                    />
                 </div>
             </div>
             <div className={styles.fullScreenImageContainer}>
                 <div className={styles.fullScreenImage}>
-                    IMAGE THREE
+                    <Image
+                        placeholder="blur"
+                        quality={50}
+                        responsive={"true"}
+                        style={{
+                            height: "100%",
+                            width: "auto",
+                        }}
+                        // objectFit="contain"
+                        src={leave_blood}
+                        alt={"an abstract painting"}
+                    />
                 </div>
             </div>
             <div className={styles.fullScreenImageContainer}>
                 <div className={styles.fullScreenImage}>
-                    IMAGE FOUR
+                    <Image
+                        placeholder="blur"
+                        // quality={50}
+                        responsive={"true"}
+                        // objectFit={"contain"}
+                        // layout={"responsive"}
+                        style={{
+                            height: "100%",
+                            width: "auto",
+                        }}
+                        src={yellow_christ}
+                        alt={"an abstract painting"}
+                    />
                 </div>
             </div>
         </main>
-        <footer className={styles.footer}>
+        {/*<footer className={styles.footer}>*/}
 
-        </footer>
+        {/*</footer>*/}
     </div>
   );
 }
